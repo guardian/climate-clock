37a04fefc4f77208c184c66ac7cd41fc03f892d3
Wai Sing Yiu <wai.sing.yiu@guardian.co.uk>
2021-09-16 12:49:00 +0100

    Initial version from springboot initializer

---
+++ src/main/java/com/gu/climateclock/ClimateclockApplication.java
@@ -0,0 +1,13 @@
+package com.gu.climateclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateclockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimateclockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gu/climateclock/ClimateclockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gu.climateclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimateclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
